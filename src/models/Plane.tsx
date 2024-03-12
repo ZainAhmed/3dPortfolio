fe6644d2d2f6501a81988b1b332710f4baaa8ba1
Zain <zain.sohail@gmail.com>
2024-03-12 14:20:54 +0100

    set plane position

--- src/models/Plane.tsx
+++ src/models/Plane.tsx
@@ -1,12 +1,19 @@
 import { useGLTF } from "@react-three/drei";
 import planeScene from "../assets/3d/plane.glb";
+import { adjustBiplaneForScreenSize } from "../utils/utils";
 function Plane() {
   const { scene, animations } = useGLTF(planeScene);
+  const [scale, position, rotation] = adjustBiplaneForScreenSize();
   return (
     <mesh>
       // use the primitive element when you want to directly embed a complex 3D
       model or scene
-      <primitive object={scene} />
+      <primitive
+        object={scene}
+        scale={scale}
+        position={position}
+        rotation={rotation}
+      />
     </mesh>
   );
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -14,12 +14,12 @@ export const adjustIslandForScreenSize = () => {
 export const adjustBiplaneForScreenSize = () => {
   let screenScale: Vector3 = [3, 3, 3];
   let screenPosition: Vector3 = [0, -4, -4];
-
+  const rotation: Vector3 = [0, 1.5, 0];
   // If screen width is less than 768px, adjust the scale and position
   if (window.innerWidth < 768) {
     screenScale = [1.5, 1.5, 1.5];
     screenPosition = [0, -1.5, 0];
   }
 
-  return [screenScale, screenPosition];
+  return [screenScale, screenPosition, rotation];
 };
