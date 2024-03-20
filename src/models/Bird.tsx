72d737d8c2742714babedbe8c86f05774c4a8e0b
Zain <zain.sohail@gmail.com>
2024-03-20 23:01:12 +0100

    fixed bird's angle with respect to island while flying around the island

--- src/models/Bird.tsx
+++ src/models/Bird.tsx
@@ -14,11 +14,10 @@ function Bird() {
     actions["Take 001"].play();
   }, []);
 
-  useFrame((state, delta) => {
-    const radius = 30; // Radius of the circular path
-    const speed = 2; // Speed of the rotation
-    const arcLength =
-      (state.clock.getElapsedTime() * speed) % (Math.PI * 2 * radius); // Angle based on time
+  useFrame((state) => {
+    const radius = 30;
+    const speed = 2;
+    const arcLength = state.clock.getElapsedTime() * speed;
     const theta = arcLength / radius;
 
     // Calculate the position of the bird along the circular path
@@ -30,24 +29,13 @@ function Bird() {
       birdRef.current.position.x = x;
       birdRef.current.position.z = z;
 
-      // Calculate the angle between the bird and the island's center
-      const angleToIsland = Math.atan2(
-        islandPosition[2] - z,
-        islandPosition[0] - x
-      );
-
-      // Adjust the rotation to face the island's center
-      birdRef.current.rotation.y = angleToIsland;
+      // Set rotation directly based on the angle of rotation
+      birdRef.current.rotation.y = -theta - Math.PI / 2; // Use theta as the angle of rotation
     }
   });
 
   return (
-    <mesh
-      position={[0, 3, -5]}
-      scale={[0.003, 0.003, 0.003]}
-      rotation={[0, 3.5, 0]}
-      ref={birdRef}
-    >
+    <mesh scale={[0.003, 0.003, 0.003]} ref={birdRef}>
       <primitive object={scene} />
     </mesh>
   );
